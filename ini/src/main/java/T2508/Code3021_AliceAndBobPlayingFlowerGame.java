848520eab1e480cbda99bbeb40c6c61e99bb82bf
iniwym <mobiwusihuan288@163.com>
2025-08-29 22:42:01 +0800

    20250829

---
+++ ini/src/main/java/T2508/Code3021_AliceAndBobPlayingFlowerGame.java
@@ -0,0 +1,22 @@
+package T2508;
+
+/**
+ * @Description: 奇偶性
+ * @Author: iniwym
+ * @Date: 2025-08-29
+ * @Link: https://leetcode.cn/problems/alice-and-bob-playing-flower-game/
+ */
+public class Code3021_AliceAndBobPlayingFlowerGame {
+
+    /**
+     * 计算花朵游戏的结果
+     *
+     * @param n 游戏参数n
+     * @param m 游戏参数m
+     * @return 返回n和m乘积的一半
+     */
+    public long flowerGame(int n, int m) {
+        return (long) n * m / 2;
+    }
+
+}
